25be43601b3290829dcd76d76b69dfd746a4655b
Peter Dave Hello <hsu@peterdavehello.org>
2020-04-30 18:06:20 +0800

    Improve and make shell script format consistent

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ freshclam -d &
 clamd &
 
 # recognize PIDs
-pidlist=`jobs -p`
+pidlist=$(jobs -p)
 
 # initialize latest result var
 latest_exit=0
@@ -21,13 +21,13 @@ function shutdown() {
     trap "" SIGINT
 
     for single in $pidlist; do
-        if ! kill -0 $single 2>/dev/null; then
-            wait $single
+        if ! kill -0 "$single" 2> /dev/null; then
+            wait "$single"
             latest_exit=$?
         fi
     done
 
-    kill $pidlist 2>/dev/null
+    kill "$pidlist" 2> /dev/null
 }
 
 # run shutdown
