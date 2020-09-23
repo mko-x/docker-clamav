fef1f8319d9a566cafddd18ff833f445c3de962f
WhiteBahamut <megagogeta_ssj4@hotmail.com>
2020-09-23 16:13:12 +0200

    again fixing stretch naming

--- build-all.sh
+++ build-all.sh
@@ -13,13 +13,14 @@ docker push ${repo}docker-clamav:buster-slim-amd64
 docker push ${repo}docker-clamav:buster-slim-armv7
 docker push ${repo}docker-clamav:buster-slim-arm64v8
 
+#armv7 cause problems in the moment
 docker build -t ${repo}docker-clamav:stretch-slim-amd64 debian/stretch/
-docker build -t ${repo}docker-clamav:stretch-slim-armv7 -f debian/stretch/Dockerfile.arm32v7 debian/stretch/
+#docker build -t ${repo}docker-clamav:stretch-slim-armv7 -f debian/stretch/Dockerfile.arm32v7 debian/stretch/
 docker build -t ${repo}docker-clamav:stretch-slim-arm64v8 -f debian/stretch/Dockerfile.arm64v8 debian/stretch/
 
-docker push ${repo}docker-clamav:strech-slim-amd64
-docker push ${repo}docker-clamav:strech-slim-armv7
-docker push ${repo}docker-clamav:strech-slim-arm64v8
+docker push ${repo}docker-clamav:stretch-slim-amd64
+#docker push ${repo}docker-clamav:stretch-slim-armv7
+docker push ${repo}docker-clamav:stretch-slim-arm64v8
 
 if ! test -f manifest-tool ; then
   curl -Lo manifest-tool https://github.com/estesp/manifest-tool/releases/download/v1.0.3/manifest-tool-linux-amd64
@@ -33,6 +34,6 @@ fi
     --target ${repo}docker-clamav:buster-slim
     
 ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm/v7,linux/arm64/v8 \
+    --platforms linux/amd64,linux/arm64/v8 \
     --template ${repo}docker-clamav:stretch-slim-ARCHVARIANT \
     --target ${repo}docker-clamav:stretch-slim
