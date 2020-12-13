8f7e61995214f4e11b4fcf4cc68015ad80e41584
mko-x <github@m-ko.de>
2020-12-13 22:51:28 +0100

    modified push logic in sh to ensure logins

--- push-all.sh
+++ push-all.sh
@@ -22,34 +22,4 @@ docker push ${repo}docker-clamav:alpine-edge-amd64
 docker push ${repo}docker-clamav:alpine-edge-armv7
 docker push ${repo}docker-clamav:alpine-edge-arm64v8
 
-if ! test -f manifest-tool ; then
-  echo Ensure compatible version of manifest tool https://github.com/estesp/manifest-tool
-  curl -Lo manifest-tool https://github.com/estesp/manifest-tool/releases/download/v1.0.3/manifest-tool-linux-amd64
-  chmod +x manifest-tool
-fi
-
-./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm/v7,linux/arm64/v8 \
-    --template ${repo}docker-clamav:buster-slim-ARCHVARIANT \
-    --target ${repo}docker-clamav:buster-slim
-
-./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm/v7,linux/arm64/v8 \
-    --template ${repo}docker-clamav:buster-slim-ARCHVARIANT \
-    --target ${repo}docker-clamav:latest
-    
-./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm64/v8 \
-    --template ${repo}docker-clamav:stretch-slim-ARCHVARIANT \
-    --target ${repo}docker-clamav:stretch-slim
-
-./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm/v7,linux/arm64/v8 \
-    --template ${repo}docker-clamav:alpine-ARCHVARIANT \
-    --target ${repo}docker-clamav:alpine
-
-./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm/v7,linux/arm64/v8 \
-    --template ${repo}docker-clamav:alpine-edge-ARCHVARIANT \
-    --target ${repo}docker-clamav:alpine-edge
-    
+echo "Push to docker registry finished."
