ff40ebfd842b465b105fa47e6325b5d76d0836cf
mko-x <github@m-ko.de>
2021-05-26 18:20:08 +0200

    Added scripts for build and push idb image

---
+++ build-idb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t mkodockx/docker-clamav:alpine-idb-amd64-v2021-05-25 -f alpine/main-idb/Dockerfile alpine/main-idb/
---
+++ push-idb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker push mkodockx/docker-clamav:alpine-idb-amd64-v2021-05-25
