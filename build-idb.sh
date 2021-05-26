29e8a41e70eac27a9a8f7d0d6029756290e58c4b
mko-x <github@m-ko.de>
2021-05-26 18:25:18 +0200

    Update build and push to general tag

--- build-idb.sh
+++ build-idb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker build -t mkodockx/docker-clamav:alpine-idb-amd64-v2021-05-25 -f alpine/main-idb/Dockerfile alpine/main-idb/
+
+docker build -t mkodockx/docker-clamav:alpine-idb-amd64 -f alpine/main-idb/Dockerfile alpine/main-idb/
--- push-idb.sh
+++ push-idb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker push mkodockx/docker-clamav:alpine-idb-amd64-v2021-05-25
+
+docker push mkodockx/docker-clamav:alpine-idb-amd64
