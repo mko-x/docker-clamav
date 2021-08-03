0457908098821b4f2d5caa9f754f26a4b39ae3c1
davecharles <22290851+davecharles@users.noreply.github.com>
2021-08-03 17:17:04 +0100

    fix: cold start race condition.

--- debian/buster/bootstrap.sh
+++ debian/buster/bootstrap.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 # bootstrap clam av service and clam av database updater shell script
 # presented by mko (Markus Kosmal<dude@m-ko.de>)
-set -m
+set -m  # job control
 
 # Configure freshclam.conf and clamd.conf from env variables if present
 source /envconfig.sh
 
+# Check if this is a first time run
+# The virus definition database is stored in ``/var/lib/clamav``.
+# Use ``-v clamdb:/var/lib/clamav`` option with docker run command
+# to persist and avoid download of db every time we start the daemon.
+if [ -z "$(ls -A /var/lib/clamav)" ]; then
+    # Run freshclam in foreground to avoid container exiting
+    # while we get fresh defs.
+    echo "No ClamAV database - running freshclam for first time"
+    freshclam
+else
+    echo "ClamAV database found!"
+fi
+
 # Start clamd service in background
+echo "Starting clamd in background"
 clamd &
 
 # Based on https://superuser.com/a/917073/66341
@@ -15,7 +29,7 @@ wait_file() {
   local file="$1"; shift
   local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
 
-  echo "Waiting $wait_seconds for $file"
+  echo "Waiting $wait_seconds seconds for $file"
 
   until test $((wait_seconds--)) -eq 0 -o -S "$file" ; 
   do
@@ -30,8 +44,8 @@ wait_file "$LOCKFILE" 60 || {
     >&2 echo "$LOCKFILE not found after waiting for 60 seconds. There may be issues with updating virus defs"
 }
 
-# Start the updater in background as daemon
-echo "Starting freshclam"
+# Clamd should be up, start the updater in background as daemon
+echo "Starting freshclam in background"
 freshclam -d &
 
 # recognize PIDs
